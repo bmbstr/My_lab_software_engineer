4131090dea5efc887e291d24e13f9624cdc32e62
bmbstr <lyuba.dovzhenko@gmail.com>
2026-04-07 15:28:55 +0200

    Fix CI/CD and k6 tests

--- tests/smoke_test.js
+++ tests/smoke_test.js
@@ -11,7 +11,7 @@ export const options = {
 };
 
 export default function () {
-    const res = http.get('http://localhost:8081');
+    const res = http.get('http://localhost:80');
     check(res, { 'status is 200': (r) => r.status === 200 });
     sleep(1);
 }
